a0b644f5fdff08412f44879fb331d6fdef7a6ff2
ErikaGalanCont <102776229+ErikaGalanCont@users.noreply.github.com>
2023-02-03 18:43:08 -0500

    solucion4

---
+++ todo_Sobre_Variables_01-02-2023/main.js
@@ -0,0 +1,13 @@
+
+let telefono = 3874839;
+
+let apellido = "galan";
+
+let afiliada = true;
+
+const cedula = 3993;
+
+console.log(telefono);
+console.log(apellido);
+console.log(afiliada);
+console.log(cedula);
